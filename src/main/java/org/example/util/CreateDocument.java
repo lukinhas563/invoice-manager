39458a9333a07200fcf8499e123a77e0bd216b30
Lucas Montenegro <lucasmontenegro475@gmail.com>
2024-05-05 16:54:03 -0300

    :goal_net: fix: Fix verification logic in the CreateDocument class

--- src/main/java/org/example/util/CreateDocument.java
+++ src/main/java/org/example/util/CreateDocument.java
@@ -5,12 +5,21 @@ import org.w3c.dom.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CreateDocument {
 
     Document document;
 
     public boolean setDocument(String path) {
+        Path file = Paths.get(path);
+
+        if (!Files.exists(file) && !path.toLowerCase().endsWith(".xml")) {
+            return false;
+        }
+
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(false);
 

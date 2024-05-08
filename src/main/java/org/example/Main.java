be3f43769052963fc5211c6139acd25453011aea
Lucas Montenegro <lucasmontenegro475@gmail.com>
2024-05-08 09:51:31 -0300

    :lock: cleanup: Remove url string

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,13 @@ import org.example.factory.serviceFactory.ServiceInvoiceFactory;
 import org.example.factory.tableFactory.TableFactory;
 import org.example.model.invoice.Service;
 
-import java.time.LocalDate;
 import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        String url = "C:/Users/cedec/OneDrive/Área de Trabalho/Lucas/nfs";
+        String url = ;
         Director director = new Director(new DocumentFactory(), new ServiceInvoiceFactory());
 
         director.createDocumentByDirectory(url);
